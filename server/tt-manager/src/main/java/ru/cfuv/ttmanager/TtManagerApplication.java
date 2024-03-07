ba48c287950443b422a4f9cb877791d0e3bca78a
Aleksio02 <oleks1o@yandex.ru>
2024-03-07 20:31:26 +0300

    [CS-B-003, CS-B-004, CS-B-006] Создан модуль менеджера расписания

---
+++ server/tt-manager/src/main/java/ru/cfuv/ttmanager/TtManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.cfuv.ttmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtManagerApplication.class, args);
+    }
+
+}
---
+++ server/tt-manager/src/test/java/ru/cfuv/ttmanager/TtManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cfuv.ttmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
