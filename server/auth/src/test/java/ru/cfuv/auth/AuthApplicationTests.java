d9f3ee1bf43b65ed9bff132833262547faaf0471
Aleksio02 <oleks1o@yandex.ru>
2024-03-07 20:28:43 +0300

    [CS-B-003, CS-B-004, CS-B-006] Создан модуль авторизации

--- server/auth/src/test/java/ru/cfuv/auth/AuthApplicationTests.java
+++ server/auth/src/test/java/ru/cfuv/auth/AuthApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.cfuv.cfuvscheduling;
+package ru.cfuv.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CfuvschedulingApplicationTests {
+class AuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
