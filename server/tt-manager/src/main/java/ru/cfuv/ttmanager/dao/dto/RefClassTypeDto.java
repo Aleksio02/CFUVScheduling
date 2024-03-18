b1e27900160a290abee9ec881170ccaca925fb78
LisenkoIA <vanyalisenko1985@gmail.com>
2024-03-18 22:07:00 +0300

    Создал сущность "Справочник типов занятий"

---
+++ server/tt-manager/src/main/java/ru/cfuv/ttmanager/dao/dto/RefClassTypeDto.java
@@ -0,0 +1,13 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ref_class_type")
+public class RefClassTypeDto {
+
+    @Id
+    private Integer id;
+    private String name;
+
+}
