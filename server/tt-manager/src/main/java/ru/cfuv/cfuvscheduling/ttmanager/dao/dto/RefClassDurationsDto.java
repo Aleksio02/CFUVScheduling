beac67538b0f83d8f1bd1d3d62aa29294a82a5fe
LisenkoIA <vanyalisenko1985@gmail.com>
2024-03-22 23:15:54 +0300

    Подредачил анотации

--- server/tt-manager/src/main/java/ru/cfuv/cfuvscheduling/ttmanager/dao/dto/RefClassDurationsDto.java
+++ server/tt-manager/src/main/java/ru/cfuv/cfuvscheduling/ttmanager/dao/dto/RefClassDurationsDto.java
@@ -1,16 +1,17 @@
 package ru.cfuv.cfuvscheduling.ttmanager.dao.dto;
 
 import jakarta.persistence.*;
+
 import java.util.Date;
 
 @Entity
-@Table(schema = "public", name = "ref_class_duration")
-
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"start_time", "end_time"})}, schema = "public", name = "ref_class_duration")
 public class RefClassDurationsDto {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer number;
+    @Column(name = "start_time", nullable = false)
     private Date startTime;
+    @Column(name = "end_time", nullable = false)
     private Date endTime;
-
 }
--- server/tt-manager/src/main/java/ru/cfuv/cfuvscheduling/ttmanager/dao/dto/RefClassTypeDto.java
+++ server/tt-manager/src/main/java/ru/cfuv/cfuvscheduling/ttmanager/dao/dto/RefClassTypeDto.java
@@ -1,14 +1,16 @@
 package ru.cfuv.cfuvscheduling.ttmanager.dao.dto;
 
 import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 
 @Entity
 @Table(schema = "public", name = "ref_class_type")
 public class RefClassTypeDto {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, unique = true)
     private String name;
 
 }
