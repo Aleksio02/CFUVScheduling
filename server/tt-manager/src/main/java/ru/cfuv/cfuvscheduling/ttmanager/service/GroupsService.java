c956ded0f3fed6a3133bbeac3b31623bcaa19f2e
Дарья <dar3018@yandex.ru>
2024-03-31 00:32:48 +0300

    [CS-B-026] Реализовала получение списка всех групп. Теперь Service возвращает BOM.

--- server/tt-manager/src/main/java/ru/cfuv/cfuvscheduling/ttmanager/service/GroupsService.java
+++ server/tt-manager/src/main/java/ru/cfuv/cfuvscheduling/ttmanager/service/GroupsService.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.cfuv.cfuvscheduling.commons.dao.GroupsDao;
 import ru.cfuv.cfuvscheduling.commons.dao.dto.GroupsDto;
+import ru.cfuv.cfuvscheduling.ttmanager.bom.GroupsBom;
+import ru.cfuv.cfuvscheduling.ttmanager.converter.GroupsConverter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +16,17 @@ public class GroupsService {
     @Autowired
     private GroupsDao groupsDao;
 
-    public List<GroupsDto> findAll() {
-        return groupsDao.findAll();
+    public List<GroupsBom> findAll() {
+        List<GroupsDto> dtos = groupsDao.findAll();
+        List<GroupsBom> boms = new ArrayList<GroupsBom>();
+
+        for (int i = 0; i < dtos.size(); i++) {
+            GroupsBom bom = new GroupsBom();
+            new GroupsConverter().fromDTO(dtos.get(i), bom);
+            boms.add(bom);
+        }
+
+        return boms;
     }
 
 }
