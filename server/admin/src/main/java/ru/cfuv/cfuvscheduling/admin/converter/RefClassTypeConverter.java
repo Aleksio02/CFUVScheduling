e28b9f873297c63df4601111be79a250c4b70903
Дарья <dar3018@yandex.ru>
2024-03-31 00:23:45 +0300

    [CS-B-026] Реализовала получение списка всех типов занятий. Теперь Service возвращает BOM.

--- server/admin/src/main/java/ru/cfuv/cfuvscheduling/admin/converter/RefClassTypeConverter.java
+++ server/admin/src/main/java/ru/cfuv/cfuvscheduling/admin/converter/RefClassTypeConverter.java
@@ -1,10 +1,11 @@
 package ru.cfuv.cfuvscheduling.admin.converter;
 
+import ru.cfuv.cfuvscheduling.admin.bom.RefClassTypeBom;
 import ru.cfuv.cfuvscheduling.commons.dao.dto.RefClassTypeDto;
 
 public class RefClassTypeConverter {
 
-    public void fromDto(RefClassTypeDto source, RefClassTypeDto destination) {
+    public void fromDto(RefClassTypeDto source, RefClassTypeBom destination) {
         destination.setId(source.getId());
         destination.setName(source.getName());
     }
--- server/admin/src/main/java/ru/cfuv/cfuvscheduling/admin/service/RefClassTypeService.java
+++ server/admin/src/main/java/ru/cfuv/cfuvscheduling/admin/service/RefClassTypeService.java
@@ -2,10 +2,12 @@ package ru.cfuv.cfuvscheduling.admin.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.cfuv.cfuvscheduling.admin.bom.RefClassTypeBom;
+import ru.cfuv.cfuvscheduling.admin.converter.RefClassTypeConverter;
 import ru.cfuv.cfuvscheduling.commons.dao.RefClassTypeDao;
 import ru.cfuv.cfuvscheduling.commons.dao.dto.RefClassTypeDto;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class RefClassTypeService {
@@ -13,8 +15,17 @@ public class RefClassTypeService {
     @Autowired
     private RefClassTypeDao refClassTypeDao;
 
-    public List<RefClassTypeDto> findAll() {
-        return refClassTypeDao.findAll();
+    public List<RefClassTypeBom> findAll() {
+        List<RefClassTypeDto> dtos = refClassTypeDao.findAll();
+        List<RefClassTypeBom> boms = new ArrayList<RefClassTypeBom>();
+
+        for (int i = 0; i < dtos.size(); i++) {
+            RefClassTypeBom bom = new RefClassTypeBom();
+            new RefClassTypeConverter().fromDto(dtos.get(i), bom);
+            boms.add(bom);
+        }
+
+        return boms;
     }
 
 }
