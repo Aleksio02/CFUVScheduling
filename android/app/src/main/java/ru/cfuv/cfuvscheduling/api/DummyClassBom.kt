19075a57cf449efcf220d08db6417a93f3ec824f
Alexander Baransky <alexander.baranskiy@yandex.ru>
2024-04-02 19:48:07 +0300

    android: Rework TimetableScreen
    New nice design
    DummyClassBom: Convert to dataclass

--- android/app/src/main/java/ru/cfuv/cfuvscheduling/api/DummyClassBom.kt
+++ android/app/src/main/java/ru/cfuv/cfuvscheduling/api/DummyClassBom.kt
@@ -2,13 +2,11 @@ package ru.cfuv.cfuvscheduling.api
 
 
 // TODO: aleksio: Заменить в будущем на что-то вменяемое (сделано для примера)
-class DummyClassBom {
-    lateinit var classCode: String
-    lateinit var className: String
-    lateinit var classType: String
-    lateinit var teacherContactName: String
-    lateinit var room: String
-    lateinit var classNumber: String
-    lateinit var startTime: String
-    lateinit var finishTime: String
-}
+data class DummyClassBom (
+    val className: String,
+    val isPractical: Boolean,
+    val teacherName: String,
+    val room: String,
+    val startTime: String,
+    val finishTime: String
+)
