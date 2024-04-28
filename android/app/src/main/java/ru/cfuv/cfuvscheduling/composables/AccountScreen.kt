222bad9ab86481bea3e674ecf9b6b43d4f79f2c0
Alexander Baransky <alexander.baranskiy@yandex.ru>
2024-04-28 12:05:09 +0300

    [CS-M-047] Allow to open login screen from account page

--- android/app/src/main/java/ru/cfuv/cfuvscheduling/composables/AccountScreen.kt
+++ android/app/src/main/java/ru/cfuv/cfuvscheduling/composables/AccountScreen.kt
@@ -2,10 +2,14 @@ package ru.cfuv.cfuvscheduling.composables
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Surface
@@ -17,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -30,29 +35,56 @@ import ru.cfuv.cfuvscheduling.api.DummyAccountInfoModel
 import ru.cfuv.cfuvscheduling.R
 
 val ACCOUNT = DummyAccountInfoModel(
-    "Антон ФранSSен",
+    "SanyaPilot",
     "Студент"
 )
 
 @Composable
-fun AccountScreen(viewModel: MainViewModel = viewModel()) {
+fun AccountScreen(
+    viewModel: MainViewModel = viewModel(),
+    onNavigateToLogin: () -> Unit
+) {
     val context = LocalContext.current
     LaunchedEffect(Unit) {
         viewModel.setAppBarTitle(context.getString(R.string.accountBarTitle))
     }
 
     Column {
-        Column(
-            modifier = Modifier.padding(horizontal = 16.dp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(
-                text = ACCOUNT.name,
-                fontSize = 32.sp
-            )
-            Text(
-                text = stringResource(id = R.string.accountRole, ACCOUNT.role),
-                fontSize = 20.sp
-            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = ACCOUNT.name,
+                    fontSize = 32.sp
+                )
+                Text(
+                    text = stringResource(id = R.string.accountRole, ACCOUNT.role),
+                    fontSize = 20.sp
+                )
+            }
+            FilledTonalButton(
+                onClick = { onNavigateToLogin() },
+                modifier = Modifier.padding(end = 16.dp),
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    end = 24.dp,
+                    top = 8.dp,
+                    bottom = 8.dp
+                )
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.rounded_login_24),
+                    contentDescription = null,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Text(text = stringResource(id = R.string.login))
+            }
         }
         var notificationsEnabled by rememberSaveable { mutableStateOf(false) }
         ListItem(
@@ -64,11 +96,6 @@ fun AccountScreen(viewModel: MainViewModel = viewModel()) {
                 .padding(top = 8.dp)
                 .clickable { notificationsEnabled = !notificationsEnabled } // TODO: notifications logics
         )
-        ListItem(
-            headlineContent = { Text(text = stringResource(id = R.string.accountLogout)) },
-            leadingContent = { Icon(painter = painterResource(id = R.drawable.logout_24), contentDescription = null) },
-            modifier = Modifier.clickable {} // TODO: logout logics
-        )
     }
 }
 
@@ -78,6 +105,6 @@ fun AccountScreenPreview() {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        AccountScreen()
+        AccountScreen(onNavigateToLogin = {})
     }
 }
