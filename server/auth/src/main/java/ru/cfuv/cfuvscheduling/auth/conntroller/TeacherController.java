0b77916cc21dc5944a75465a1587ba5975b38821
LisenkoIA <vanyalisenko1985@gmail.com>
2024-04-29 15:09:22 +0300

    [CS-B-028] Убрал лишние импорты

--- server/auth/src/main/java/ru/cfuv/cfuvscheduling/auth/conntroller/TeacherController.java
+++ server/auth/src/main/java/ru/cfuv/cfuvscheduling/auth/conntroller/TeacherController.java
@@ -3,12 +3,10 @@ package ru.cfuv.cfuvscheduling.auth.conntroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.cfuv.cfuvscheduling.auth.service.AuthService;
 import ru.cfuv.cfuvscheduling.auth.service.TeacherService;
 import ru.cfuv.cfuvscheduling.commons.bom.UserBom;
-import ru.cfuv.cfuvscheduling.commons.bom.UserRoles;
 import ru.cfuv.cfuvscheduling.commons.exception.AccessForbiddenException;
 
 
